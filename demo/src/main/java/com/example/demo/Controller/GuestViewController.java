0782d1d75ba2472346ca1005f8aa0601d5faa266
Martynas Slavinskas <Martynas.Slavinskas@gmail.com>
2022-08-21 15:52:16 +0300

    Added the ability to create guests and delete them

--- demo/src/main/java/com/example/demo/Controller/GuestViewController.java
+++ demo/src/main/java/com/example/demo/Controller/GuestViewController.java
@@ -1,9 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Guest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/guests")
@@ -21,4 +25,20 @@ public class GuestViewController {
         return "guests";
     }
 
+    @GetMapping("/newGuest")
+    public String getGuestCreationForm(){
+        return "addGuest";
+    }
+
+    @PostMapping
+    public String createGuest(Guest guest){
+        guestController.saveGuest(guest);
+        return "redirect:/guests";
+    }
+
+    @RequestMapping(value = "/deleteGuest/{id}", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String deleteGuest(@PathVariable("id") Long id){
+        guestController.deleteGuest(id);
+        return "redirect:/guests";
+    }
 }
